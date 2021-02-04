62aa9a72978437ae38af6fa0bb0b697245b6b52c
redapple0414 <77644441+redapple0414@users.noreply.github.com>
2021-02-04 16:42:41 +0900

    Initial commit

---
+++ Assets/Point.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Point : MonoBehaviour
+{
+    private GameObject tensuuText;
+
+    private float visiblePosZ = -6.5f;
+
+    private int tokuten;
+
+    private GameObject gameoverText;
+
+    private int finish = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        this.tensuuText = GameObject.Find("tensuu");
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+      
+            
+        this.tensuuText.GetComponent<Text>().text = "Point:" + this.tokuten;
+        if (this.tokuten >= 100)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Clear";
+            this.finish = 1;
+        }
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            if (this.finish == 0)
+            {
+                //GameoverTextにゲームオーバを表示
+                this.gameoverText.GetComponent<Text>().text = "Game Over";
+            }
+        }
+        
+    }
+
+
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.tokuten += 1;
+        }
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.tokuten += 3;
+        }
+        if (other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            this.tokuten += 5;
+        }
+        if (other.gameObject.tag == "bad")
+        {
+            this.tokuten -= 3;
+        }
+
+        Debug.Log(tokuten);
+
+       
+    }
+
+}
---
+++ Assets/ozyamamusi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ozyamamusi : MonoBehaviour
+{
+    private float angle = 2.0f;
+    float speed = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        
+        
+        Vector3 velocity = gameObject.transform.rotation * new Vector3(0, 0, speed);
+        gameObject.transform.position += velocity * Time.deltaTime;
+        if (this.transform.position.z < -6.5f || this.transform.position.z > 6.5f　|| this.transform.position.y < -6.5f)
+        {
+            transform.position = new Vector3(3f, 5f, 4f);
+            
+        }
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+
+
+        this.angle = Random.Range(10, 30);
+        this.transform.Rotate(0, this.angle, 0);
+    }
+}
